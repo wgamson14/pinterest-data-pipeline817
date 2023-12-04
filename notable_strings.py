99643693a6f09d0a8634a611b71c86745fadc9b6
Will Gamson <willgamson@Wills-Air.Home>
2023-12-04 17:51:45 +0000

    setting up .pem file and configuring EC2 Kafka client

---
+++ notable_strings.py
@@ -0,0 +1,8 @@
+Account_ID = 584739742957
+
+user_ID = '1272e2b5acdf'
+
+zookeeper_connection = "z-2.pinterestmskcluster.w8g8jt.c12.kafka.us-east-1.amazonaws.com:2181,z-1.pinterestmskcluster.w8g8jt.c12.kafka.us-east-1.amazonaws.com:2181,z-3.pinterestmskcluster.w8g8jt.c12.kafka.us-east-1.amazonaws.com:2181"
+
+boostrap_brokers = "b-1.pinterestmskcluster.w8g8jt.c12.kafka.us-east-1.amazonaws.com:9098,b-3.pinterestmskcluster.w8g8jt.c12.kafka.us-east-1.amazonaws.com:9098,b-2.pinterestmskcluster.w8g8jt.c12.kafka.us-east-1.amazonaws.com:9098"
+
